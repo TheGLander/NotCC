70dff978cc9f36ec33df25b61003e55e97a955b0
Zee <lubomir.yudenko@gmail.com>
2023-03-09 16:08:14 +0200

    Remove unused UI files

--- gamePlayer/src/ui/levelList.ts
+++
@@ -1,8 +0,0 @@
-import { setPlayer } from "./setPlayer"
-
-export const levelList = document.querySelector<HTMLSelectElement>("#levelList")
-
-levelList?.addEventListener("change", () => {
-	setPlayer.currentLevelIndex = parseInt(levelList.value)
-	setPlayer.restartLevel()
-})
