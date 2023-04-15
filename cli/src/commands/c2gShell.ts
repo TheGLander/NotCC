89a5d8485eb24c38a999c13f2f22d3ad31881593
Zee <lubomir.yudenko@gmail.com>
2023-04-15 18:23:14 +0300

    Rewrite the CLI C2G shell

--- cli/src/commands/c2gShell.ts
+++ cli/src/commands/c2gShell.ts
@@ -1,76 +1,66 @@
-import { C2GRunner, tokenizeC2G, C2G_NOTCC_VERSION } from "@notcc/logic"
+import { ScriptRunner, C2G_NOTCC_VERSION, ScriptInterrupt } from "@notcc/logic"
 import { errorAndExit } from "../helpers"
-import prompts from "prompts"
-const { prompt } = prompts
+import { createInterface } from "readline"
 import fs from "fs"
 import { resolve } from "path"
-import chalk from "chalk"
+import pc from "picocolors"
 import { Argv } from "yargs"
+
+const inf = createInterface({ input: process.stdin, output: process.stdout })
+function question(prompt: string): Promise<string> {
+	return new Promise(res => {
+		inf.question(prompt, ans => res(ans))
+	})
+}
+
+function resolveInterrupt(runner: ScriptRunner, interrupt: ScriptInterrupt) {
+	if (interrupt.type === "chain") {
+		const c2gPath = resolve(
+			process.cwd(),
+			interrupt.path.replace(/^~/, process.env.HOME ?? "")
+		)
+
+		if (!fs.existsSync(c2gPath)) {
+			errorAndExit("A chain path must lead to a file!")
+		}
+		const c2gData = fs.readFileSync(c2gPath, "latin1")
+		runner.handleChainInterrupt(c2gData)
+		return
+	} else if (interrupt.type === "script") {
+		console.log(interrupt.text)
+		runner.scriptInterrupt = null
+		return
+	} else if (interrupt.type === "map") {
+		console.warn("`map` interrupts aren't supported in the shell.")
+		runner.handleMapInterrupt({ type: "skip" })
+		return
+	}
+}
+
 async function startC2GShell(): Promise<void> {
 	console.log(
-		chalk(`{green NotCC C2G Shell version {bold ${C2G_NOTCC_VERSION}}}`)
+		pc.green(`NotCC C2G Shell version ${pc.bold(`${C2G_NOTCC_VERSION}`)}`)
 	)
-	const runner = new C2GRunner(tokenizeC2G('game "NotCC shell"'))
-	runner.stepLine()
-	function resolveQueuedAction() {
-		for (
-			let action = runner.queuedActions.shift();
-			action;
-			action = runner.queuedActions.shift()
-		)
-			switch (action.type) {
-				case "chain": {
-					const c2gPath = resolve(
-						process.cwd(),
-						action.value.replace(/^~/, process.env.HOME ?? "")
-					)
+	const runner = new ScriptRunner('game "C2G shell"')
+	runner.executeLine()
 
-					if (!fs.existsSync(c2gPath))
-						errorAndExit("A chain path must lead to a file!")
-					runner.tokens = tokenizeC2G(
-						fs.readFileSync(c2gPath, { encoding: "latin1" })
-					)
-					runner.currentToken = 0
-					runner.updateLabels()
-					while (runner.tokens[runner.currentToken]) {
-						runner.stepLine()
-						resolveQueuedAction()
-					}
-					break
-				}
-				case "script":
-					console.log(action.value)
-
-					break
-				default:
-					console.warn("The CLI doesn't support this type of action!")
-					break
-			}
-	}
 	// eslint-disable-next-line no-constant-condition
 	while (true) {
-		// eslint-disable-next-line no-constant-condition
-		while (true) {
-			const commandScript = await prompt(
-				{
-					type: "text",
-					name: "command",
-					message: "",
-				},
-				{ onCancel: () => errorAndExit() }
-			)
-			runner.tokens.push(
-				...tokenizeC2G(commandScript.command.replace(/\\$/, "") + "\n")
-			)
-			if (!commandScript.command.endsWith("\\")) break
+		const newScriptLines: string[] = []
+		let scriptLine: string
+		let firstLine = true
+		do {
+			scriptLine = await question(firstLine ? "> " : ". ")
+			newScriptLines.push(scriptLine.replace(/\\$/, ""))
+			firstLine = false
+		} while (scriptLine.endsWith("\\"))
+		runner.scriptLines.push(...newScriptLines)
+		runner.generateLabels()
+		let interrupt: ScriptInterrupt | null
+
+		while ((interrupt = runner.executeUntilInterrupt())) {
+			resolveInterrupt(runner, interrupt)
 		}
-		runner.updateLabels()
-		let latestValue: number | void
-		while (runner.tokens[runner.currentToken]) {
-			latestValue = runner.stepLine()
-			resolveQueuedAction()
-		}
-		console.log(latestValue)
 	}
 }
 
