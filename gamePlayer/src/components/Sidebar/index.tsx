4d9e4ee69578c394344572ac76a22b2a69990d48
Zee <lubomir.yudenko@gmail.com>
2023-11-29 16:26:56 +0200

    Add dummy sidebar choosers (tooltip and drawer)

--- gamePlayer/src/components/Sidebar/index.tsx
+++ gamePlayer/src/components/Sidebar/index.tsx
@@ -1,14 +1,112 @@
-import { ComponentChildren } from "preact"
+import { ComponentChildren, ComponentProps, Ref } from "preact"
 import leafIcon from "./tabIcons/leaf.svg"
 import levelIcon from "./tabIcons/level.svg"
 import floppyIcon from "./tabIcons/floppy.svg"
 import clockIcon from "./tabIcons/clock.svg"
 import toolsIcon from "./tabIcons/tools.svg"
 import infoIcon from "./tabIcons/info.svg"
+import { useLayoutEffect, useRef, useState } from "preact/hooks"
+import { forwardRef } from "preact/compat"
+import { twJoin } from "tailwind-merge"
+import { useMediaQuery } from "react-responsive"
+
+function useSidebarChooserAnim<T extends HTMLElement>(
+	open: boolean
+): {
+	ref: Ref<T>
+	closingAnim: boolean
+	endClosingAnim: () => void
+	shouldRender: boolean
+} {
+	const [wasOpen, setWasOpen] = useState(false)
+	if (!wasOpen && open) {
+		setWasOpen(true)
+	}
+
+	const ref = useRef<T>(null)
+	const [closingAnim, setClosingAnim] = useState(false)
+
+	useLayoutEffect(() => {
+		if (wasOpen && !open) {
+			setClosingAnim(true)
+			// REFLOW the main div so that the new animation plays
+			void ref.current?.offsetHeight
+		}
+	}, [wasOpen, open])
+	function endClosingAnim() {
+		if (closingAnim) {
+			setWasOpen(false)
+			setClosingAnim(false)
+		}
+	}
+	return { ref, closingAnim, endClosingAnim, shouldRender: open || closingAnim }
+}
+
+const SidebarTooltip = forwardRef<HTMLDialogElement, ComponentProps<"dialog">>(
+	function SidebarTooltip(props, fref) {
+		const { endClosingAnim, closingAnim, ref, shouldRender } =
+			useSidebarChooserAnim<HTMLDivElement>(!!props.open)
+		if (!shouldRender) return
+
+		return (
+			<div
+				class={twJoin(
+					"absolute left-full top-1/3 z-10 flex [transform-origin:theme(spacing.2)_theme(spacing.2)]",
+					props.open && "animate-tooltip-open",
+					closingAnim && "animate-tooltip-close"
+				)}
+				onAnimationEnd={endClosingAnim}
+				ref={ref}
+			>
+				<div class="border-r-theme-900 mr-[-4px] inline-block h-0 w-0 border-8 border-transparent" />
+				<dialog {...props} open tabIndex={0} ref={fref} class="box static" />
+			</div>
+		)
+	}
+)
+
+function applyRef<T>(ref: Ref<T>, val: T | null): void {
+	if (typeof ref === "function") ref(val)
+	else if (ref) ref.current = val
+}
+
+const SidebarDrawer = forwardRef<HTMLDialogElement, ComponentProps<"dialog">>(
+	function SidebarDrawer(props, fref) {
+		const { endClosingAnim, closingAnim, ref, shouldRender } =
+			useSidebarChooserAnim<HTMLDialogElement>(!!props.open)
+		if (!shouldRender) return
+
+		return (
+			<dialog
+				{...props}
+				open
+				ref={dialog => {
+					applyRef(ref, dialog)
+					applyRef(fref, dialog)
+				}}
+				onAnimationEnd={endClosingAnim}
+				class={twJoin(
+					"box fixed bottom-20 left-0 right-0 z-10 mx-auto w-screen rounded-b-none shadow-none [transform-origin:0_100%]",
+					props.open && "animate-drawer-open",
+					closingAnim && "animate-drawer-close"
+				)}
+			/>
+		)
+	}
+)
 
 function SidebarButton(
 	props: { icon: string } | { children: ComponentChildren }
 ) {
+	const [tooltipOpened, setTooltipOpened] = useState(false)
+	const onDialogMount = (dialog: HTMLDialogElement | null) => {
+		if (tooltipOpened && dialog) {
+			dialog.focus()
+		}
+	}
+	const useDrawer = !useMediaQuery({ query: "(min-width: 768px)" })
+	const SidebarChooser = useDrawer ? SidebarDrawer : SidebarTooltip
+
 	return (
 		<div class="relative flex md:[&:nth-last-child(2)]:mt-auto">
 			{"icon" in props ? (
@@ -16,11 +114,21 @@ function SidebarButton(
 					tabIndex={0}
 					draggable={false}
 					src={props.icon}
-					class="m-auto block cursor-pointer select-none max-md:h-3/5 md:w-4/5"
+					class="m-auto block cursor-pointer select-none max-md:h-4/5 md:w-3/5"
+					onClick={() => {
+						setTooltipOpened(true)
+					}}
 				/>
 			) : (
 				props.children
 			)}
+			<SidebarChooser
+				open={tooltipOpened}
+				onfocusout={() => setTooltipOpened(false)}
+				ref={onDialogMount}
+			>
+				Todo!
+			</SidebarChooser>
 		</div>
 	)
 }
--- gamePlayer/tailwind.config.js
+++ gamePlayer/tailwind.config.js
@@ -13,6 +13,34 @@ export default {
 					)
 				),
 			},
+			animation: {
+				"tooltip-open": "0.1s ease-in-out tooltip-reveal",
+				"tooltip-close": "0.1s ease-in-out tooltip-reveal reverse",
+				"drawer-open": "0.05s ease-in-out drawer-reveal",
+				"drawer-close": "0.05s ease-in-out drawer-reveal reverse",
+			},
+			keyframes: {
+				"tooltip-reveal": {
+					from: {
+						transform: "scale(0.4)",
+						opacity: "0",
+					},
+					to: {
+						transform: "scale(1)",
+						opacity: "1",
+					},
+				},
+				"drawer-reveal": {
+					from: {
+						opacity: 0.7,
+						transform: "scaleY(0%)",
+					},
+					to: {
+						opacity: 1,
+						transform: "scaleY(100%)",
+					},
+				},
+			},
 		},
 	},
 	plugins: [],
